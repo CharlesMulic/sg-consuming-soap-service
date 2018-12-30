cd4aa5a3bf1acdc1ecfc8137af2f69fdca5fec33
Charlie <charles.mulic@gmail.com>
2018-12-30 16:15:22 -0600

    initial

---
+++ src/main/java/com/charliemulic/sg/consumingsoapservice/ConsumingSoapServiceApplication.java
@@ -0,0 +1,14 @@
+package com.charliemulic.sg.consumingsoapservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingSoapServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumingSoapServiceApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/charliemulic/sg/consumingsoapservice/ConsumingSoapServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.charliemulic.sg.consumingsoapservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConsumingSoapServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
